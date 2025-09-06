897f1cb956622e8eff456e310b67b36157dcd418
Maharshi Patel <maharship@jfrog.com>
2025-09-06 15:43:03 -0700

    Added jsonwebtoken

--- JFTD-110-GitHub_Actions_for_JFrog/helloworld_npm/helloworld.js
+++ JFTD-110-GitHub_Actions_for_JFrog/helloworld_npm/helloworld.js
@@ -1,4 +1,8 @@
-var http = require('http');
+let http = require('http');
+let jwt = require('jsonwebtoken');
+
+
+const decoded = jwt.verify(token, CONFIG.jwt_secret_key);
 
 http.createServer(function(req, res) {
   res.writeHead(200, {'Content-Type' : 'text/plain'});
